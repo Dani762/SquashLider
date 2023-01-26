d0fc8ec3a4261d6cd95ec10f0675c0b5ff659278
danielgomezperez <daniobey24@gmail.com>
2023-01-26 19:57:46 +0100

    Todos los cambios

--- Squash.java
+++ Squash.java
@@ -1,7 +1,13 @@
 public class Squash {
     //A
     //B
+<<<<<<< HEAD
     //colaborador 1
     //colaborador2
     //colaborador 3
+=======
+    //lider 1
+    //lider 2
+    //lider 3
+>>>>>>> lider
 }
